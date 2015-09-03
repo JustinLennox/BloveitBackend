d453220c3cf19c928bf1e39dcd4c5ceb98cf874d
Justin Lennox <justinanthonylennox@gmail.com>
2015-09-02 21:07:23 -0400

    Fix Auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
-#This baby right here uses the Facebook App ID and Secret found on the facebook
-#page. The ID is 881886568566728, and the secret is 99b637633339cdec158aff53f4e3a9f7
+#Facebook initializer. Passes the app id (881886568566728) and secret (99b637633339cdec158aff53f4e3a9f7) as STRINGS
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 881886568566728, 99b637633339cdec158aff53f4e3a9f7
+  provider :facebook, ENV['881886568566728'], ENV['99b637633339cdec158aff53f4e3a9f7']
 end
